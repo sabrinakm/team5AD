5bfd902f48dfcb74ccc9cc0d2a3482d6e907a66a
sabrinakm <e0390028@u.nus.edu>
2019-07-24 09:14:51 +0800

    test branch push

---
+++ StationeryStore/StationeryStore/Data/Data.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace StationeryStore.Data
+{
+    public class Data
+    {
+        // set connectionstring to server and database
+        public static string connectionString = "Server=SK;" +
+                "Database=stationeryStore; Integrated Security=true";
+    }
+}
